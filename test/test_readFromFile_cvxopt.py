621f7a85c4fd97a5e2bdd490f939cf19c416c8c5
salini <josephsalini@gmail.com>
2012-05-08 22:01:25 +0200

    fix cvxopt test and add some test files

--- test/test_readFromFile_cvxopt.py
+++ test/test_readFromFile_cvxopt.py
@@ -2,35 +2,46 @@
 #author=joseph salini
 #date=30 april 2012
 
-from numpy import array, dot
+from numpy import array, dot, zeros
 
 from cvxopt import solvers, matrix
 from cvxopt.solvers import qp as qpsolver
 
-f = open("/home/joe/myProblem.txt", "r")
+import sys
+
+_fileName = sys.argv[1]
+f = open(_fileName, "r")
 problemtext = f.read()
 f.close()
 
-print problemtext
+print "==============> "+_fileName+" <=============="
 
 Problist = problemtext.split()
-
-print Problist
 
 nProblem = int(Problist[Problist.index('nProblem:')+1])
 nEquality = int(Problist[Problist.index('nEquality:')+1])
 nInequality = int(Problist[Problist.index('nInequality:')+1])
 
-print nProblem, nEquality, nInequality
+P = zeros((nProblem, nProblem))
+q = zeros(nProblem)
+if "P:" in Problist:
+    P = array([float(v) for v in Problist[Problist.index('P:')+1 : Problist.index('P:')+ nProblem*nProblem +1]]).reshape(nProblem,nProblem)
+if "q:" in Problist:
+    q = array([float(v) for v in Problist[Problist.index('q:')+1 : Problist.index('q:')+ nProblem +1]]).reshape(nProblem)
 
-P = array([float(v) for v in Problist[Problist.index('P:')+1 : Problist.index('P:')+ nProblem*nProblem +1]]).reshape(nProblem,nProblem)
-q = array([float(v) for v in Problist[Problist.index('q:')+1 : Problist.index('q:')+ nProblem +1]]).reshape(nProblem)
+A = zeros((nEquality, nProblem))
+b = zeros(nEquality)
+if "A:" in Problist:
+    A = array([float(v) for v in Problist[Problist.index('A:')+1 : Problist.index('A:')+ nEquality*nProblem +1]]).reshape(nEquality,nProblem)
+if "b:" in Problist:
+    b = array([float(v) for v in Problist[Problist.index('b:')+1 : Problist.index('b:')+ nEquality +1]]).reshape(nEquality)
 
-A = array([float(v) for v in Problist[Problist.index('A:')+1 : Problist.index('A:')+ nEquality*nProblem +1]]).reshape(nEquality,nProblem)
-b = array([float(v) for v in Problist[Problist.index('b:')+1 : Problist.index('b:')+ nEquality +1]]).reshape(nEquality)
-
-G = array([float(v) for v in Problist[Problist.index('G:')+1 : Problist.index('G:')+ nInequality*nProblem +1]]).reshape(nInequality,nProblem)
-h = array([float(v) for v in Problist[Problist.index('h:')+1 : Problist.index('h:')+ nInequality +1]]).reshape(nInequality)
+G = zeros((nInequality, nProblem))
+h = zeros(nInequality)
+if "G:" in Problist:
+    G = array([float(v) for v in Problist[Problist.index('G:')+1 : Problist.index('G:')+ nInequality*nProblem +1]]).reshape(nInequality,nProblem)
+if "h:" in Problist:
+    h = array([float(v) for v in Problist[Problist.index('h:')+1 : Problist.index('h:')+ nInequality +1]]).reshape(nInequality)
 
 print "P:\n", P
 print "q:\n", q
@@ -54,3 +65,5 @@ X_sol = array(results['x']).flatten()
 
 print "cost:", .5*dot(X_sol, dot(P, X_sol)) + dot(q, X_sol)
 print X_sol
+
+
